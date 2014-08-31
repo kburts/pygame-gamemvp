f35131e73ceee72f384e23231a00a613fe3c4fda
burtonjb <burton.jeff.r@gmail.com>
2014-08-30 18:33:04 -0700

    moved some files around

---
+++ client/Map/TileMap.py
@@ -0,0 +1,12 @@
+__author__ = 'Jeff'
+
+
+import sys
+
+
+class TileMap():
+    #contains all the TileSprites for the map.
+    #responsible for CRUDing the TileSprites.
+    #SHOULD NOT be responsible for any NPC stuff
+    def __init__(self):
+        pass
--- client/Map/TileSprite.py
+++ client/Map/TileSprite.py
@@ -6,17 +6,21 @@ import tmxlib
 from PIL import Image
 
 
+TILE_SIZE_PIXELS = 32
+RESOURCES = 'resources//'
+
+
 class TileSprite(pygame.sprite.Sprite):
-    def __init__(self, image, location, tilesize):
+    def __init__(self, image, location):
         super(TileSprite, self).__init__()
-        location = (location[0] * tilesize, location[1] * tilesize)
+        location = (location[0], location[1])
         left = location[0]
         top = location[1]
-        right = location[0] + 32
-        bottom = location[1] + 32
+        right = location[0] + TILE_SIZE_PIXELS
+        bottom = location[1] + TILE_SIZE_PIXELS
 
-        im = Image.open(resources_folder + image).crop((left, top, right, bottom))
-        self.image = pygame.image.fromstring(im.tostring("raw", "RGB"), im.size, "RGB")
+        image = Image.open(RESOURCES + image).crop((left, top, right, bottom))
+        self.image = pygame.image.fromstring(image.tostring("raw", "RGB"), image.size, "RGB")
         self.rect = pygame.rect.Rect(location, self.image.get_size())
 
 if __name__ == '__main__':
@@ -24,35 +28,31 @@ if __name__ == '__main__':
     size = width, height = 640, 480
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
+    map = tmxlib.Map.open(RESOURCES + 'map-blocker-sides.tmx')
 
-    resources_folder = 'resources//'
-    map = tmxlib.Map.open(resources_folder + 'map-blocker-sides.tmx')
     tiles = list(map.layers[0].all_tiles())[:400]
     print vars(tiles[0])
     print tiles[0], tiles[-1]
 
-    tilesize = 32
-    num_tiles = (width/tilesize, height/tilesize)
+    num_tiles = (width/TILE_SIZE_PIXELS, height/TILE_SIZE_PIXELS)
     visible_tiles = []
 
     sprite_tile_group = pygame.sprite.Group()
 
     for x in range(num_tiles[0]):
-        #visible_tiles.append([]);
         for y in range(num_tiles[1]):
             if map.layers[0].__getitem__((x,y)).image is not None:
                 image = map.layers[0].__getitem__((x,y)).image.image.source
-                spritesheet_pos = map.layers[0].__getitem__((x,y))
+                sprite_sheet_pos = map.layers[0].__getitem__((x,y))
                 location = map.layers[0].__getitem__((x,y))._pos
-                #location = map.layers[0].__getitem__((x,y)).image.top_left
-                sprite_tile_group.add(TileSprite(image, location, tilesize, spritesheet_pos))
+                sprite_tile_group.add(TileSprite(image, location))
 
-            #visible_tiles[-1].append(map.layers[0].__getitem__([x, y]))
-            #sprite_tile_group.add(TileSprite())
-    print sprite_tile_group
-    while 1:
+    while True:
         dt = clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                sys.exit()
         sprite_tile_group.draw(screen)
         pygame.display.flip()
-
-    print 'done'
